9cf5ac5be8dabb67beda5e6ae8f9f18b8bd3ea62
blavelle-portfolio <106982674+blavelle-portfolio@users.noreply.github.com>
2022-12-29 14:59:52 -0500

    Update esxi.sh

--- system_scripts/esxi.sh
+++ system_scripts/esxi.sh
@@ -1,9 +1,9 @@
-virt-install 
---name esxi01 \
---ram 16512 --arch=x86_64 \
---vcpus=8 --cpu host --check-cpu \
---disk path=/home/user/iso/esxi01.img,qcow2,size=400 \
---location  /home/user/imgs/VMware-VMvisor-Installer-8.0-20513097.x86_64.iso \
---graphics none \
---console pty,target_type=serial \
---extra-args 'console=ttyS0,115200n8'
+virt-install --virt-type=kvm --name=vmware8 \
+  --ram 72876 --vcpus=20 \
+  --virt-type=kvm --hvm \
+  --cdrom /home/owner/imgs/VMware-VMvisor-Installer-8.0-20513097.x86_64.iso \
+  --network model=E1000 \
+  --graphics vnc \
+  --disk path=/home/owner/kvm_guests/esxi8.img,size=400,sparse=true,bus=sata,format=qcow2 \
+  --boot cdrom,hd  \
+  --cpu host-passthrough --os-variant linux2020
