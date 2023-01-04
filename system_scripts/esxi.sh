af72d383dc3e33bcaf791d2ae9b83ac70227130b
blavelle-portfolio <106982674+blavelle-portfolio@users.noreply.github.com>
2023-01-03 19:49:14 -0500

    Update esxi.sh

--- system_scripts/esxi.sh
+++ system_scripts/esxi.sh
@@ -1,9 +1,10 @@
 virt-install --virt-type=kvm --name=vmware8 \
-  --ram 72876 --vcpus=20 \
-  --virt-type=kvm --hvm \
-  --cdrom /home/owner/imgs/VMware-VMvisor-Installer-8.0-20513097.x86_64.iso \
+  --ram 62785 --vcpus=12 \
+  --cdrom /home/owner/VMware-VMvisor-Installer-7.iso \
   --network model=E1000 \
+  --os-variant linux2020 \
   --graphics vnc \
-  --disk path=/home/owner/kvm_guests/esxi8.img,size=400,sparse=true,bus=sata,format=qcow2 \
+  --disk path=/home/owner/vms/esxi7.qcow2,size=900,sparse=true,bus=sata,format=qcow2 \
   --boot cdrom,hd  \
-  --cpu host-passthrough --os-variant linux2020
+  --cpu host-passthrough \
+  --features kvm_hidden=on
